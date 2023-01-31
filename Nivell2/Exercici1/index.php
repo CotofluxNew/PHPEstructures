01aa7c615ddd0b2f51869aea77f049421aa893be
CotofluxNew <cotoflux@gmail.com>
2023-01-31 13:20:18 +0100

    Corretgit el nivell 2 exercici1 creada funció

--- Nivell2/Exercici1/index.php
+++ Nivell2/Exercici1/index.php
@@ -2,14 +2,15 @@
 /*
 Ens han demanat un llistat de cada any on es van produir jocs olímpics des de 1960 inclòs fins al 2016. Programa un bucle que calculi i mostri per pantalla els anys olímpics dins de l'interval esmentat. */
 
-$anyInici = 1960;
-$anyFi = 2016;
-$frequencia = 4;
+
+function calculaAnysJocsOlimpics($anyInici, $anyFi){
+    $frequencia = 4;
 
     while ($anyFi > $anyInici) {
         $anyInici = $anyInici + $frequencia;
         echo $anyInici."<br/>";
     }
+}
 
-
+calculaAnysJocsOlimpics(1960, 2016);
 ?>
