59cb12c391ee0215e62691feccc1646d024335d5
CotofluxNew <cotoflux@gmail.com>
2023-01-31 09:40:50 +0100

    Feta funció segons demanat

--- Exercici2/index.php
+++ Exercici2/index.php
@@ -2,8 +2,16 @@
 /*
 Per jugar a l'"amagatall" se'ns ha demanat un programa que compti fins a 10. Però la persona que comptarà és una mica tramposa i ho farà comptant de dos en dos. Crea una funció que compti fins a 10, de 2 en 2, mostrant cada número del compte per pantalla.*/
 
+// for ($i = 0; $i <= 10; $i += 2) {
+//     echo "El número ", $i, "<br/>";
+// }
+
+function ferTrampa($i){
+    echo "Numero ".$i."<br/>";
+}
+
 for ($i = 0; $i <= 10; $i += 2) {
-    echo "El número ", $i, "<br/>";
+     ferTrampa($i);
 }
 
 
